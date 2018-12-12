9b5c63eee0faa858b7222f1d7a7c760ba8e02652
groovy-sky <40501227+groovy-sky@users.noreply.github.com>
2018-12-12 09:11:24 +0200

    Update script.sh

--- ansible-tower-00/script.sh
+++ ansible-tower-00/script.sh
@@ -74,6 +74,12 @@ az vm extension set --resource-group $deployment_group --vm-name $vm_name --name
 az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"0,/#listen 80/{s/#listen 80/listen 80/}\" /etc/nginx/sites-enabled/default;"}'
 az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"0,/#listen\s\[::\]/{s/#listen\s\[::\]/listen [::]/}\" /etc/nginx/sites-enabled/default;"}'
 
+az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"0,/root/{s/root/#root/}\" /etc/nginx/sites-enabled/default;"}'
+az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"/^[[:space:]]root/d\" /etc/nginx/sites-enabled/default;"}'
+
+az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"0,/location/{s/location/#location/}\" /etc/nginx/sites-enabled/default;"}'
+
+
 az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"/ssl_dhparam/alocation \/ \{ \n proxy_set_header Host $host; \n proxy_pass http\:\/\/127.0.0.1:8080; \n \} \" /etc/nginx/sites-enabled/default;"}'
 
 az vm extension set --resource-group $deployment_group --vm-name $vm_name --name customScript --publisher Microsoft.Azure.Extensions --settings '{  "commandToExecute": "sudo sed -i \"/ssl_dhparam/alocation \/ \{ \\n proxy_set_header Host \$host; \\n proxy_pass http\:\/\/127.0.0.1:8080; \\n \} \" /etc/nginx/sites-enabled/default;"}'
